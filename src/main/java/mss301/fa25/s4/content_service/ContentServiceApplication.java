01e3072b519c4c2d23d830676e950ac4668b18d4
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-14 11:12:51 +0700

    feat: setup project base for content svc

---
+++ src/main/java/mss301/fa25/s4/content_service/ContentServiceApplication.java
@@ -0,0 +1,13 @@
+package mss301.fa25.s4.content_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentServiceApplication.class, args);
+	}
+
+}
