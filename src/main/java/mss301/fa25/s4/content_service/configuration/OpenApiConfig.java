4a902c3f9f3a0e8f25ed26eb470c047d2ce41303
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-16 13:14:03 +0700

    fix: swagger config

--- src/main/java/mss301/fa25/s4/content_service/configuration/OpenApiConfig.java
+++ src/main/java/mss301/fa25/s4/content_service/configuration/OpenApiConfig.java
@@ -30,7 +30,7 @@ public class OpenApiConfig {
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .servers(List.of(
                         new Server()
-                                .url("http://localhost:8080")
+                                .url("http://localhost:8080/content-service")
                                 .description("API Gateway"),
                         new Server()
                                 .url("http://localhost:8085/content")
