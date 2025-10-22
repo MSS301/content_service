78d53c6e04e7ee3960ff6c20a90296a0a70b6de1
sonnamnguyen <sonnamsonnam402@gmail.com>
2025-10-22 21:36:02 +0700

    feat: pagination for lessonFile

--- src/main/java/mss301/fa25/s4/content_service/repository/LessonFileRepository.java
+++ src/main/java/mss301/fa25/s4/content_service/repository/LessonFileRepository.java
@@ -2,13 +2,18 @@ package mss301.fa25.s4.content_service.repository;
 
 import mss301.fa25.s4.content_service.entity.LessonFile;
 import mss301.fa25.s4.content_service.constant.EntityStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface LessonFileRepository extends JpaRepository<LessonFile, Integer> {
+public interface LessonFileRepository extends JpaRepository<LessonFile, Integer>, PagingAndSortingRepository<LessonFile, Integer> {
     List<LessonFile> findByLessonIdAndStatus(Integer lessonId, EntityStatus status);
+    Page<LessonFile> findByLessonIdAndStatus(Integer lessonId, EntityStatus status, Pageable pageable);
     List<LessonFile> findByUploaderIdAndStatus(Integer uploaderId, EntityStatus status);
+    Page<LessonFile> findByUploaderIdAndStatus(Integer uploaderId, EntityStatus status, Pageable pageable);
 }
